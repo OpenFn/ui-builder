cfa0dfb6863b828528fa4f3902ee91aa7aab852c
Stuart Corbishley <corbish@gmail.com>
2022-02-08 16:03:11 +0200

    Allow initial code to be set via constructor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './main.css'
 // rename to a 'component'.
 import App from './App.svelte'
+import { code } from './sourceFileStore';
 
-export function Builder(elem: HTMLElement): App {
+export function Builder(elem: HTMLElement, initialCode: string): App {
+  if (initialCode) {
+    code.update(() => initialCode);
+  }
+
   const app = new App({ target: elem })
   return app
 }
